30004784f121264165baba298ef12a819f14b020
Halimove01 <halim.dakir@gmail.com>
2020-02-12 13:59:11 +0100

    ceate new class

--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -1,17 +1,8 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
 import java.util.StringTokenizer;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -20,7 +11,7 @@ public class HTTPServer implements Runnable{
     static final String INDEX = "index.html";
     static final String ERROR_404 = "404.html";
     static final String NOT_SUPPORTED = "not_supported.html";
-    static final File FILE_ROOT = new File(".");
+    static final File FILE_ROOT = new File(".\\resources");
     static final boolean prolix = true;
     private Socket socket;
 
@@ -35,6 +26,7 @@ public class HTTPServer implements Runnable{
         PrintWriter out = null;
         BufferedOutputStream dataOut = null;
         String fileRequested = null;
+
 
         try {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -55,18 +47,8 @@ public class HTTPServer implements Runnable{
                             int fileLength = (int) file.length();
                             String contentMimeType = "text/html";
                             //read content to return to client
-                            byte[] fileData = readData(file, fileLength);
-                            // Send HTTP Headers
-                            out.println("HTTP/1.1 Not Implemented - HTTP 501 Error");
-                            out.println("Server: Java HTTP Server 1.0");
-                            out.println("Date: " + new Date());
-                            out.println("Content-type: " + contentMimeType);
-                            out.println("Content-length: " + fileLength);
-                            out.println();
-                            out.flush();
-
-                            dataOut.write(fileData, 0, fileLength);
-                            dataOut.flush();
+                            byte[] fileData = HandleServer.getInstance().readData(file, fileLength);
+                            HandleServer.getInstance().hTTP501Error(out, dataOut, fileData, contentMimeType, fileLength);
 
             } else {
                 if (fileRequested.endsWith("/")) {
@@ -75,21 +57,10 @@ public class HTTPServer implements Runnable{
 
                 File file = new File(FILE_ROOT, fileRequested);
                 int fileLength = (int) file.length();
-                String content = getTypeOfContent(fileRequested);
+                String content = HandleServer.getInstance().getTypeOfContent(fileRequested);
                 if (methodString.equals("GET")) {
-                    byte[] fileData = readData(file, fileLength);
-
-                    // send HTTP Headers
-                    out.println("HTTP/1.1 200 OK");
-                    out.println("Server: Java HTTP Server 1.0");
-                    out.println("Date: " + new Date());
-                    out.println("Content-type: " + content);
-                    out.println("Content-length: " + fileLength);
-                    out.println();
-                    out.flush();
-
-                    dataOut.write(fileData, 0, fileLength);
-                    dataOut.flush();
+                    byte[] fileData = HandleServer.getInstance().readData(file, fileLength);
+                    HandleServer.getInstance().hTTP200Ok(out, dataOut, fileData, content, fileLength);
                 }
 
                 if (prolix) {
@@ -100,7 +71,7 @@ public class HTTPServer implements Runnable{
 
         } catch (FileNotFoundException e) {
             try {
-                fileNotFound(out, dataOut, fileRequested);
+                HandleServer.getInstance().fileNotFound(FILE_ROOT, ERROR_404, out, dataOut, fileRequested);
             } catch (IOException ioe) {
                 System.err.println("Error with file not found! exception : " + ioe.getMessage());
             }
@@ -125,62 +96,7 @@ public class HTTPServer implements Runnable{
 
     }
 
-    private byte[] readData(File file, int fileLength) throws IOException {
-        FileInputStream fileIn = null;
-        byte[] fileData = new byte[fileLength];
 
-        try {
-            fileIn = new FileInputStream(file);
-            fileIn.read(fileData);
-        } finally {
-            if (fileIn != null)
-                fileIn.close();
-        }
-
-        return fileData;
-    }
- // support only HTML
-
-    private String getTypeOfContent(String fileRequested) {
-        if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
-            return "text/html";
-        else if (fileRequested.endsWith(".pdf"))
-            return "application/pdf";
-        else if (fileRequested.endsWith(".css"))
-            return "text/css";
-        else if (fileRequested.endsWith(".jpeg"))
-        return "image/jpeg";
-        else if (fileRequested.endsWith(".jpg"))
-            return "image/jpg";
-        else if (fileRequested.endsWith(".png") || fileRequested.endsWith(".PNG"))
-            return "image/png";
-        else if (fileRequested.endsWith(".json"))
-            return "application/json";
-        else if (fileRequested.endsWith(".js"))
-            return "application/javascript";
-        else
-            return "text/plain";
-    }
-
-    private void fileNotFound(PrintWriter out, OutputStream dataOut, String fileRequested) throws IOException {
-        File file = new File(FILE_ROOT, ERROR_404);
-        int fileLength = (int) file.length();
-        String content = "text/html";
-        byte[] fileData = readData(file, fileLength);
-        out.println("HTTP/1.1 404 File Not Found");
-        out.println("Server: Java HTTP Server 1.0");
-        out.println("Date: " + new Date());
-        out.println("Content-type: " + content);
-        out.println("Content-length: " + fileLength);
-        out.println();   // blank line is very important between header and content!
-        out.flush();
-        dataOut.write(fileData, 0, fileLength);
-        dataOut.flush();
-
-        if (prolix) {
-            System.out.println("File " + fileRequested + " not found");
-        }
-    }
 
     public static void main(String[] args) {
         try {
